e14d5dbb67a82828ae3780b7314c3d125f4706bd
Deepak Mandal <demanda1@in.ibm.com>
2019-12-31 15:47:05 +0530

    link changed

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 //Install express server
 const express = require('express');
 const path = require('path');
- 
+const cors = require('cors');
 const app = express();
  
 // Serve only the static files form the angularapp directory
+app.use(cors());
 app.use(express.static(__dirname + '/angularapp'));
  
 app.get('/*', function(req,res) {
